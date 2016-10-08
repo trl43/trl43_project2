14ecd9b261ae993167e7ad9c9cdfec782bba438c
Locker,Todd <trl43@254-13.cs.uakron.edu>
2016-10-08 14:27:19 -0400

    Initial Commit

---
+++ trl43_project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trl43_project2
+//
+//  Created by Locker,Todd on 10/8/16.
+//  Copyright © 2016 Locker,Todd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
