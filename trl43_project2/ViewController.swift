76e2c7865f626de98cafdb4cd561dbb245da9122
Locker,Todd <trl43@254-13.cs.uakron.edu>
2016-10-08 15:32:41 -0400

    Added objects to the UI

--- trl43_project2/ViewController.swift
+++ trl43_project2/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
