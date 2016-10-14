bae810476fd5b7012472c3ab2c0866aae50a7a4d
Locker,Todd <trl43@254-13.cs.uakron.edu>
2016-10-13 21:43:07 -0400

    Added action to message area and butons

--- trl43_project2/ViewController.swift
+++ trl43_project2/ViewController.swift
@@ -10,21 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var Button1: UIButton!
-    @IBOutlet weak var Button2: UIButton!
-    @IBOutlet weak var Button3: UIButton!
-    @IBOutlet weak var Button4: UIButton!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var message: UITextView!
+    @IBOutlet var wholeView: UIView!
+    @IBOutlet var mainViewTapped: UITapGestureRecognizer!
+    @IBOutlet weak var submitButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // shifts the view up for space for the keyboard
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardWillShow, object: nil, queue: nil) {
+            
+            notification in
+            
+            print("HEREEEEEEEEEEEE\(notification.description)")
+            
+            // find the size of the keyboard
+            guard let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue else {
+                print("ERROR: Unable to get keyboard size")
+                return
+            }
+            
+            // move the whole thing up
+            self.view.frame.origin.y = -keyboardSize.height
+        }
+        
+        
+        // shift the view back down
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardWillHide, object: nil, queue: nil) {
+            
+            notification in
+            
+            print("THEREEEEEEEEEEEEEE\(notification.description)")
+            
+            // move back to original position
+            self.view.frame.origin.y = 0
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func topButtonPressed(_ sender: UIButton) {
+        self.topButton.isSelected = true
+        self.topButton.backgroundColor = UIColor.green
     }
-
-
+    
+    @IBAction func submitPressed(_ sender: AnyObject) {
+        self.view.endEditing(true)
+        self.view.frame.origin.y = 0
+    }
+    
+    @IBAction func mainViewTapped(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func messageTapped(_ sender: UITapGestureRecognizer) {
+        message.becomeFirstResponder()
+        message.textColor = UIColor.black
+        message.selectedTextRange = message.textRange(from: message.beginningOfDocument, to: message.endOfDocument)
+    }
+    
 }
 
